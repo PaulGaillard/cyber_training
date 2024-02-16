283c768c24bbac807f0dd254a3aba6b38882bf02
PaulGaillard <paul.gaillard.86190@gmail.com>
2024-02-16 17:26:50 +0100

    Update authbypass2.php to edit url

--- authbypass2.php
+++ authbypass2.php
@@ -1,8 +1,8 @@
 <?php
 
-$url="/example/authbypass2.php";
+$url="/".basename(__DIR__)."/authbypass2.php";
 $requested_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-if($url === $requested_uri){
+if(strcasecmp($url, $requested_uri) === 0){
 	http_response_code(403);
 	echo "ERROR 403 - UNAUTHORIZED";
 	exit();
@@ -26,6 +26,7 @@ Header( 'Expires: Tue, 23 Jun 2009 12:00:00 GMT' );     // Date in the past
 
 	<body>
 	<h3> Seul un super administrateur a accès à cette page ! </h3>
+	<p> Atttention ce test ne fonction que si vous êtes dans niveau n-1 de votre arborescence web (exemple qui fonctionne : http://localhost/example/authbypass2.php & exemple qui ne fonctionne pas : http://localhost/dir/example/authbypass2.php). Dans le cas où ça ne fonctionne pas, remplacer $url dans le code source par votre URI (exemple : /dir/example/authbypass2.php)<p>
 	</body>
 
 </html>
